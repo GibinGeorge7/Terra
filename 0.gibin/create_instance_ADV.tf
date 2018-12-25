407270b28adcebc0755f6885aaf43eb8eb89edbd
GibinGeorge7 <gibin.g@gmail.com>
2018-12-25 10:02:34 +0400

    sync work 25-dec-18

---
+++ 0.gibin/create_instance_ADV.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+#query the AWS API for the latest Ubuntu Xenial AMI
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+#define our instance
+resource "aws_instance" "my_first_instance" {
+    ami           = "${data.aws_ami.latest-ubuntu.id}"
+    instance_type = "t2.micro"
+
+    tags = {
+    Name = "Terraform_aws1"
+  }
+
+}
+
