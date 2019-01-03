587c020e99901b481263f90814013ed3f26c05fb
GibinGeorge7 <gibin.g@gmail.com>
2019-01-03 10:37:19 +0400

    sync new work

--- 0.gibin/working.tf
+++ 0.gibin/working.tf
@@ -1,38 +1,41 @@
-# create EC2 Instance 
-# create VPC ,subnets,route table , IGW
-# create security group - IN /OUT
-
-
 ##################################################################################
 # VARIABLES
 ##################################################################################
 
-variable "aws_acckey" {}
-variable "aws_seckey" {}
-variable "aws_region" {default ="us-east-1"}
-variable "aws_prikey_path" {}
-variable "aws_pubkey_name" { default = "AWS-EC2-1"}
-variable "vpc_cidr1" {default = "10.1.0.0/16"}
-variable "vpc_subnet1" {default = "10.1.0.0/24"}
-variable "vpc_subnet2" {default = "10.1.1.0/24"}
-variable  "ec2_ami" {default = "ami-c58c1dd3"}
-
-
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "AWS-EC2-1"
+}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
+variable "billing_code_tag" {}
+variable "environment_tag" {}
+variable "bucket_name" {}
 
 ##################################################################################
 # PROVIDERS
 ##################################################################################
 
 provider "aws" {
-  access_key = "${var.aws_acckey }"
-  secret_key = "${var.aws_seckey}"
-  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
 }
 
 ##################################################################################
-# DATASOURCE
+# DATA
 ##################################################################################
-data "aws_availability_zones" "AZ" {}
+
+data "aws_availability_zones" "available" {}
 
 ##################################################################################
 # RESOURCES
@@ -40,31 +43,52 @@ data "aws_availability_zones" "AZ" {}
 
 # NETWORKING #
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.vpc_cidr1}"
-  enable_dns_hostnames = "true"
-  tags { Name ="Terra VPC"}
+  cidr_block = "${var.network_address_space}"
+
+  tags {
+    Name = "${var.environment_tag}-vpc"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.vpc.id}"
-  tags { Name ="Terra IGW"}
+
+  tags {
+    Name = "${var.environment_tag}-igw"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
 
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block        = "${var.vpc_subnet1}"
+  cidr_block        = "${var.subnet1_address_space}"
   vpc_id            = "${aws_vpc.vpc.id}"
   map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.AZ.names[0]}"
-  tags { Name ="Terra SUBNET1"}
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "${var.environment_tag}-subnet1"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
 }
 
 resource "aws_subnet" "subnet2" {
-  cidr_block        = "${var.vpc_subnet2}"
+  cidr_block        = "${var.subnet2_address_space}"
   vpc_id            = "${aws_vpc.vpc.id}"
   map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.AZ.names[1]}"
-  tags { Name ="Terra SUBNET2" }
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "${var.environment_tag}-subnet2"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
 }
 
 # ROUTING #
@@ -75,7 +99,13 @@ resource "aws_route_table" "rtb" {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.igw.id}"
   }
-  tags { Name ="Terra RouteTable"}
+
+  tags {
+    Name = "${var.environment_tag}-rtb"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
 }
 
 resource "aws_route_table_association" "rta-subnet1" {
@@ -89,11 +119,38 @@ resource "aws_route_table_association" "rta-subnet2" {
 }
 
 # SECURITY GROUPS #
+resource "aws_security_group" "elb-sg" {
+  name        = "nginx_elb_sg"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  #Allow HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.environment_tag}-elb-sg"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
+}
+
 # Nginx security group 
 resource "aws_security_group" "nginx-sg" {
   name        = "nginx_sg"
   vpc_id      = "${aws_vpc.vpc.id}"
-  tags { Name ="Terra SG"}
 
   # SSH access from anywhere
   ingress {
@@ -103,12 +160,12 @@ resource "aws_security_group" "nginx-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP access from anywhere
+  # HTTP access from the VPC
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.network_address_space}"]
   }
 
   # outbound internet access
@@ -118,6 +175,36 @@ resource "aws_security_group" "nginx-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags {
+    Name = "${var.environment_tag}-nginx-sg"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
+}
+
+# LOAD BALANCER #
+resource "aws_elb" "web" {
+  name = "nginx-elb"
+
+  subnets         = ["${aws_subnet.subnet1.id}", "${aws_subnet.subnet2.id}"]
+  security_groups = ["${aws_security_group.elb-sg.id}"]
+  instances       = ["${aws_instance.nginx1.id}", "${aws_instance.nginx2.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  tags {
+    Name = "${var.environment_tag}-elb"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
 }
 
 # INSTANCES #
@@ -126,27 +213,224 @@ resource "aws_instance" "nginx1" {
   instance_type = "t2.micro"
   subnet_id     = "${aws_subnet.subnet1.id}"
   vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
-  key_name        = "${var.aws_pubkey_name}"
-  tags { Name ="Terra_NGINX_EC1"}
+  key_name        = "${var.key_name}"
 
   connection {
     user        = "ec2-user"
-    private_key = "${file(var.aws_prikey_path)}"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    content = <<EOF
+access_key = ${aws_iam_access_key.write_user.id}
+secret_key = ${aws_iam_access_key.write_user.secret}
+use_https = True
+bucket_location = US
+
+EOF
+    destination = "/home/ec2-user/.s3cfg"
+  }
+
+  provisioner "file" {
+    content = <<EOF
+/var/log/nginx/*log {
+    daily
+    rotate 10
+    missingok
+    compress
+    sharedscripts
+    postrotate
+      INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
+      /usr/local/bin/s3cmd sync /var/log/nginx/access.log-* s3://${aws_s3_bucket.web_bucket.id}/$INSTANCE_ID/nginx/
+      /usr/local/bin/s3cmd sync /var/log/nginx/error.log-* s3://${aws_s3_bucket.web_bucket.id}/$INSTANCE_ID/nginx/
+    endscript
+}
+
+EOF
+    destination = "/home/ec2-user/nginx"
   }
 
   provisioner "remote-exec" {
     inline = [
       "sudo yum install nginx -y",
       "sudo service nginx start",
-      "echo '<html><head><title>Blue Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
+      "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
+      "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
+      "sudo pip install s3cmd",
+      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
+      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
+      "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
+      "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png",
+      "sudo logrotate -f /etc/logrotate.conf"
+      
     ]
   }
+
+  tags {
+    Name = "${var.environment_tag}-nginx1"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
+}
+
+resource "aws_instance" "nginx2" {
+  ami           = "ami-c58c1dd3"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.subnet2.id}"
+  vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
+  key_name        = "${var.key_name}"
+
+  connection {
+    user        = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    content = <<EOF
+access_key = ${aws_iam_access_key.write_user.id}
+secret_key = ${aws_iam_access_key.write_user.secret}
+use_https = True
+bucket_location = US
+
+EOF
+    destination = "/home/ec2-user/.s3cfg"
+  }
+
+  provisioner "file" {
+    content = <<EOF
+/var/log/nginx/*log {
+    daily
+    rotate 10
+    missingok
+    compress
+    sharedscripts
+    postrotate
+      INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
+      /usr/local/bin/s3cmd sync /var/log/nginx/access.log-* s3://${aws_s3_bucket.web_bucket.id}/$INSTANCE_ID/nginx/
+      /usr/local/bin/s3cmd sync /var/log/nginx/error.log-* s3://${aws_s3_bucket.web_bucket.id}/$INSTANCE_ID/nginx/
+    endscript
+}
+
+EOF
+    destination = "/home/ec2-user/nginx"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start",
+      "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
+      "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
+      "sudo pip install s3cmd",
+      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
+      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
+      "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
+      "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png",
+      "sudo logrotate -f /etc/logrotate.conf"
+      
+    ]
+  }
+
+  tags {
+    Name = "${var.environment_tag}-nginx2"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
+}
+
+# S3 Bucket config#
+resource "aws_iam_user" "write_user" {
+    name = "${var.environment_tag}-s3-write-user"
+    force_destroy = true
+}
+
+resource "aws_iam_access_key" "write_user" {
+    user = "${aws_iam_user.write_user.name}"
+}
+
+resource "aws_iam_user_policy" "write_user_pol" {
+    name = "write"
+    user = "${aws_iam_user.write_user.name}"
+    policy= <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${var.environment_tag}-${var.bucket_name}",
+                "arn:aws:s3:::${var.environment_tag}-${var.bucket_name}/*"
+            ]
+        }
+   ]
+}
+EOF
+
+}
+
+resource "aws_s3_bucket" "web_bucket" {
+  bucket = "${var.environment_tag}-${var.bucket_name}"
+  acl = "private"
+  force_destroy = true
+
+      policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadForGetBucketObjects",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.environment_tag}-${var.bucket_name}/*"
+        },
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${aws_iam_user.write_user.arn}"
+            },
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${var.environment_tag}-${var.bucket_name}",
+                "arn:aws:s3:::${var.environment_tag}-${var.bucket_name}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  tags {
+    Name = "${var.environment_tag}-web_bucket"
+    BillingCode        = "${var.billing_code_tag}"
+    Environment = "${var.environment_tag}"
+  }
+
+}
+
+resource "aws_s3_bucket_object" "website" {
+  bucket = "${aws_s3_bucket.web_bucket.bucket}"
+  key    = "/website/index.html"
+  source = "./index.html"
+
+}
+
+resource "aws_s3_bucket_object" "graphic" {
+  bucket = "${aws_s3_bucket.web_bucket.bucket}"
+  key    = "/website/Globo_logo_Vert.png"
+  source = "./Globo_logo_Vert.png"
+
 }
 
 ##################################################################################
 # OUTPUT
 ##################################################################################
 
-output "aws_instance_public_dns" {
-    value = "${aws_instance.nginx1.public_dns}"
+output "aws_elb_public_dns" {
+    value = "${aws_elb.web.dns_name}"
 }
